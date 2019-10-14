dec10e70c9b7d9922de3f923c05702730d8d645b
Yuan Wang <yuan.w0811@gmail.com>
2019-10-13 21:04:57 -0600

    exe 2

--- src/HList.hs
+++ src/HList.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE TypeOperators #-}
+{-# OPTIONS_GHC -fwarn-incomplete-patterns #-}
 
 --https://youtu.be/6snteFntvjM?t=2988
 module HList where
@@ -10,3 +11,11 @@ data HList tys where
   (:>) :: h -> HList t -> HList (h ': t)
 
 infixr 5 :>
+
+data Elem list elt where
+  EZ :: Elem (x ': xs) x
+  ES :: Elem xs x -> Elem (y ': xs) x
+
+get :: Elem tys ty -> HList tys -> ty
+get EZ (x :> y) = x
+get (ES elem) (x :> y) = get elem y
